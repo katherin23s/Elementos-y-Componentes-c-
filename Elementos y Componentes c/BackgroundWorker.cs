de8277559d2d9ebd105206de6c4e4a8d770faf3e
katherin serna meza <katherin.serna@tectijuana.edu.mx>
2020-12-08 10:55:10 -0800

    BackgroundWorker
    Como hacer una barra de progreson en c# con cancelacion

---
+++ Elementos y Componentes c/BackgroundWorker.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Elementos_y_Componentes_c
+{
+    public partial class BackgroundWorker : Form
+    {
+        public BackgroundWorker()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(backgroundWorker1.IsBusy != true)
+            {
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for(int x=1; x<=10; x++)
+            {
+                if (backgroundWorker1.CancellationPending == true) {
+                    e.Cancel = true;
+                    break;
+                }
+                Thread.Sleep(1000);
+                backgroundWorker1.ReportProgress(x * 10);
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled == true)
+                MessageBox.Show("Se cancelo");
+            else if (e.Error != null)
+                MessageBox.Show("Ocurrio un error");
+            else
+                MessageBox.Show("Se termino");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.CancelAsync();
+        }
+    }
+}
---
+++ Elementos y Componentes c/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Elementos_y_Componentes_c
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new BackgroundWorker());
+        }
+    }
+}
